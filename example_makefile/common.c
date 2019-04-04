d0df987426fd7dc7a8a789a9781c9f7c5d1f0198
PeterTh <peter@metaclassofnil.com>
2019-04-04 15:37:10 +0200

    Add example makefile

---
+++ example_makefile/common.c
@@ -0,0 +1,8 @@
+#include "common.h"
+
+#include <stdio.h>
+
+void common_function(int foo)
+{
+	printf("using common %d\n", foo);
+}
---
+++ example_makefile/program2.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+#include "common.h"
+
+int main(void)
+{
+	puts("This is Program 2");
+
+	common_function(3);
+
+	return EXIT_SUCCESS;
+}
